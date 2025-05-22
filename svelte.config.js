912869bafd21f472187138f4a69bb5b085607cb6
Jan-Tobias Sohns <sohns@rhrk.uni-kl.de>
2025-05-22 16:13:18 +0200

    add action

--- svelte.config.js
+++ svelte.config.js
@@ -7,7 +7,7 @@ const config = {
       fallback: '404.html'
     }),
     paths: {
-      base: process.argv.includes('dev') ? '' : '/Playground'
+      base: process.argv.includes('dev') ? '' : '/svelte-playground'
     }
   }
 };
